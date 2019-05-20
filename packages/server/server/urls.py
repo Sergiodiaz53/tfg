33443d2274f566c868489804349d73624f714688
sifaw <metaforaideas@gmail.com>
2019-05-21 00:24:57 +0200

    Swagger codegen from django rest framework specification

--- packages/server/server/urls.py
+++ packages/server/server/urls.py
@@ -19,11 +19,28 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from .api import urlpatterns as api_urlpatterns
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TFG API",
+        default_version="v1",
+        description="Test description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
