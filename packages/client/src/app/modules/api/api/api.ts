d1da6feba5a1e2590ce734df73a99ad4a35a3af5
sifaw <metaforaideas@gmail.com>
2019-06-04 23:33:28 +0200

    Move api module

--- packages/client/src/app/modules/api/api/api.ts
+++
@@ -1,9 +0,0 @@
-export * from './accessToken.service';
-import { AccessTokenService } from './accessToken.service';
-export * from './history.service';
-import { HistoryService } from './history.service';
-export * from './historyLine.service';
-import { HistoryLineService } from './historyLine.service';
-export * from './user.service';
-import { UserService } from './user.service';
-export const APIS = [AccessTokenService, HistoryService, HistoryLineService, UserService];
