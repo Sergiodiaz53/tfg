1a21119f0b8a9feb1ad8c815abdf2718e0e02bb5
sifaw <metaforaideas@gmail.com>
2019-06-11 22:47:40 +0200

    Close questions page when last question is answered

--- packages/client/src/app/api/model/historyLineSimple.ts
+++ packages/client/src/app/api/model/historyLineSimple.ts
@@ -14,5 +14,6 @@
 export interface HistoryLineSimple { 
     readonly id?: number;
     image: string;
+    readonly historyClosed?: string;
 }
 
--- packages/client/src/app/main/questions/questions.page.ts
+++ packages/client/src/app/main/questions/questions.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HistoryLineSimple, HistoryLineDetail } from '../../api';
 import { HistoryService } from '../../services/history/history.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { flatMap, tap, finalize } from 'rxjs/operators';
 import { LoadingController } from '@ionic/angular';
+import { Location } from '@angular/common';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-questions',
@@ -14,7 +16,10 @@ export class QuestionsPage implements OnInit {
 
   question: HistoryLineSimple;
 
+  private questionStarted: number;
+
   constructor(
+    private location: Location,
     private route: ActivatedRoute,
     private loadingController: LoadingController,
     private historyService: HistoryService
@@ -29,9 +34,15 @@ export class QuestionsPage implements OnInit {
     await loading.present();
 
     this.historyService.sendAnswer(this.question.id, {
-      answer, duration: 0
+      answer, duration: Date.now() - this.questionStarted
     }).pipe(
-      flatMap(() => this.getNextQuestion()),
+      flatMap(() => {
+        if (!this.question.historyClosed) {
+          return this.getNextQuestion()
+        } else {
+          return of(this.location.back());
+        }
+      }),
       finalize(() => loading.dismiss())
     ).subscribe();
   }
@@ -40,7 +51,10 @@ export class QuestionsPage implements OnInit {
     return this.historyService.nextQuestion(+this.route.snapshot.paramMap.get('id'))
       .pipe(
         tap(
-          question => this.question = question
+          question => {
+            this.question = question;
+            this.questionStarted = Date.now();
+          }
         )
       );
   }
